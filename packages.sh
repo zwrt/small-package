5539c12741486ef9a680c3edd106dca3c4ad9fdc
zwrt <155215126+zwrt@users.noreply.github.com>
2024-01-12 18:25:14 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -33,7 +33,7 @@ git clone --depth 1 https://github.com/xiangfeidexiaohuo/extra-ipk && mv -n extr
 git clone --depth 1 https://github.com/ophub/luci-app-amlogic amlogic && mv -n amlogic/luci-app-amlogic ./;rm -rf amlogic
 git clone --depth 1 https://github.com/sbwml/luci-app-alist openwrt-alist && mv -n openwrt-alist/*alist ./ ; rm -rf openwrt-alist
 git clone --depth 1 https://github.com/lisaac/luci-app-dockerman && mv -n luci-app-dockerman/applications/luci-app-dockerman ./ ; rm -rf luci-app-dockerman
-git clone --depth 1 https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash ./luci-app-openclash
+git clone --depth 1 https://github.com/vernesong/OpenClash && mv -n OpenClash/dev/luci-app-openclash ./ ; rm -rf luci-app-openclash
 git clone --depth 1 https://github.com/linkease/nas-packages-luci && mv -n nas-packages-luci/luci/luci-app-linkease ./ ; rm -rf nas-packages-luci
 git clone --depth 1 https://github.com/linkease/nas-packages && mv -n nas-packages/multimedia/ffmpeg-remux ./ ; rm -rf nas-packages
 git clone --depth 1 https://github.com/linkease/nas-packages && mv -n nas-packages/network/services/linkease ./ ; rm -rf nas-packages
